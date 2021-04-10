eb87419ef365d74eac874ab99575e5fddbd8e2ee
VincentH28 <73213916+VinceH28@users.noreply.github.com>
2021-04-09 22:39:14 -0400

    Update projects.js

---
+++ public/components/projects.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { createGlobalStyle } from 'styled-components';
+
+function ProjectsHeaderDisplayDisplay() {
+  return (
+    <div>
+      <h1 className="projects">Projects</h1>
+    </div>
+  );
+}
+export default ProjectsHeaderDisplayDisplay;

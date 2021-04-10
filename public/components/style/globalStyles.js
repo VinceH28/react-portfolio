20b763c4548d80db3c7d94b2ef79b36a95ec483a
VincentH28 <73213916+VinceH28@users.noreply.github.com>
2021-04-10 14:40:27 -0400

    Update globalStyles.js

--- public/components/style/globalStyles.js
+++ public/components/style/globalStyles.js
@@ -2,6 +2,29 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
 
+:root {
+    --blue: #036dc4;
+    --white: #fff;
+    --grey: #efefef;
+  }
+  
+  
+  /* Scrollbar */
+  body::-webkit-scrollbar {
+    width: 8px;
+  }
+  html {
+    scrollbar-width: auto;
+    scrollbar-color: var(--blue) var(--white);
+  }
+  body::-webkit-scrollbar-track {
+    background: var(--white);
+  }
+  body::-webkit-scrollbar-thumb {
+    background-color: var(--blue) ;
+    border-radius: 6px;
+    border: 3px solid var(--white);
+  }
 `;
 
 export default GlobalStyles;

3d6b4df0fc4c9ffc2ae797fa1281d27334278830
VincentH28 <73213916+VinceH28@users.noreply.github.com>
2021-04-09 22:39:00 -0400

    Update footer.js

---
+++ public/components/footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { createGlobalStyle } from 'styled-components';
+
+
+function Footer() {
+  return (
+    <div>
+      <footer className="navbar navbar-default navbar-fixed-bottom">
+        <p className="nav-bar-text pFooter">
+          {" "}
+          Created by Vince Hart © 2021 Copyright
+        </p>
+      </footer>
+    </div>
+  )
+}
+export default Footer;
